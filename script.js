23f37f198560e8d7650ea7c86a9e2a230b3349e6
salmonbhattirajpoot-debug <salmonbhattirajpoot@gmail.com>
2026-01-17 15:30:02 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    document.getElementById("navLinks").classList.toggle("active");
+}
+
+function submitForm(event) {
+    event.preventDefault();
+    alert("Thank you! Your message has been sent successfully.");
+}
